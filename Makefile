a3649081f5830815ff929d72672423482bc94682
araki <araki@yzrh.jp>
2024-06-06 17:01:33 +0900

    minimum test

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 
 PORT=18888
-R=1
+M=1
 W=1
+C=1
 N=1
 
 run:
-	python runner app:app --bind 0.0.0.0:${PORT} --access-logfile - --error-logfile - --max-requests ${R} --workers ${W}
+	python runner app:app --bind 0.0.0.0:${PORT} --access-logfile - --error-logfile - --max-requests ${M} --workers ${W}
 
 test:
-	ab -c ${N} -n ${N} http://localhost:${PORT}/
+	ab -c ${C} -n ${N} http://localhost:${PORT}/
+
+testauth:
+	ab -c 8 -n 8 http://localhost:8081/oauth2/token
+
+testauthtest:
+	ab -c 8 -n 8 http://localhost:8081/test
--- app.py
+++ app.py
@@ -1,10 +1,58 @@
 import time
-from fastapi import FastAPI
+import os
+import asyncio
+from starlette.applications import Starlette
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route
 
-app = FastAPI()
+
+class ASGIMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        print(f"[{os.getpid()}] ASGIMiddleware await")
+        await asyncio.sleep(1)
+        print(f"[{os.getpid()}] ASGIMiddleware call")
+        await self.app(scope, receive, send)
+        print(f"[{os.getpid()}] ASGIMiddleware done")
 
 
-@app.get("/")
-async def root():
+class HTTPMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        print(f"[{os.getpid()}] HTTPMiddleware await")
+        time.sleep(1)
+        print(f"[{os.getpid()}] HTTPMiddleware call")
+        response = await call_next(request)
+        print(f"[{os.getpid()}] HTTPMiddleware done")
+        return response
+
+
+class HTTPMiddlewareAsync(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        print(f"[{os.getpid()}] app.HTTPMiddlewareAsync await")
+        await asyncio.sleep(1)
+        print(f"[{os.getpid()}] app.HTTPMiddlewareAsync call")
+        response = await call_next(request)
+        print(f"[{os.getpid()}] app.HTTPMiddlewareAsync done")
+        return response
+
+
+async def root(request):
+    print(f"[{os.getpid()}] app.root endpoint begin")
     time.sleep(1)
-    return "OK"
+    print(f"[{os.getpid()}] app.root endpoint finish")
+    return PlainTextResponse("OK")
+
+app = Starlette(routes=[
+    Route('/', root),
+])
+
+
+# app.add_middleware(ASGIMiddleware)
+# app.add_middleware(ASGIMiddleware)
+# app.add_middleware(HTTPMiddleware)
+app.add_middleware(HTTPMiddlewareAsync)
+app.add_middleware(HTTPMiddlewareAsync)
